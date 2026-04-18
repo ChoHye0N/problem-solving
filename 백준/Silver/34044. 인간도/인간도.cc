040e43a1006566d0de9f51b446783429b89e5846
ChoHyeon <yunnk29@naver.com>
2026-04-18 12:19:28 +0900

    [Silver II] Title: 인간도, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/34044. 인간도/인간도.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n == 1) {
+        cout << -1;
+        return 0;
+    }
+
+    if (n % 2 == 0) {
+        for (int i = 0; i < n / 2; i++) {
+            cout << "1331";
+        }
+    }
+    else {
+        for (int i = 0; i < (n - 3) / 2; i++) {
+            cout << "1331";
+        }
+        cout << "313137";
+    }
+
+    return 0;
+}
