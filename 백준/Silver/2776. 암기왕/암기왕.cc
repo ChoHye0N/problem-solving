2b3ab8c56795a9184289024ac387f22f6527f70a
ChoHyeon <yunnk29@naver.com>
2026-04-20 14:29:46 +0900

    [Silver IV] Title: 암기왕, Time: 1196 ms, Memory: 81572 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.cc
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -1,26 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int bs(vector<int>& arr, int n) {
-    int s = 0, e = arr.size() - 1;
-
-    while (s <= e) {
-        int mid = (s + e) / 2;
-
-        if (arr[mid] == n) {
-            return 1;
-        }
-        else if (arr[mid] > n) {
-            e = mid - 1;
-        }
-        else {
-            s = mid + 1;
-        }
-    }
-
-    return 0;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
@@ -29,7 +9,7 @@ int main() {
     cin >> t;
 
     while (t--) {
-        vector<int> arr, brr;
+        unordered_map<int, int> mrr;
         int n, m;
 
         cin >> n;
@@ -37,7 +17,7 @@ int main() {
             int tmp;
             cin >> tmp;
 
-            arr.push_back(tmp);
+            mrr[tmp]++;
         }
 
         cin >> m;
@@ -45,13 +25,7 @@ int main() {
             int tmp;
             cin >> tmp;
 
-            brr.push_back(tmp);
-        }
-
-        sort(arr.begin(), arr.end());
-
-        for (int i = 0; i < m; i++) {
-            cout << bs(arr, brr[i]) << '\n';
+            cout << (mrr[tmp] >= 1 ? 1 : 0) << '\n';
         }
     }
 
