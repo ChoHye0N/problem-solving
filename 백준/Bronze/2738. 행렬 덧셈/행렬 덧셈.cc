dad2e1943456273c48bc155966a5705020fa7159
ChoHyeon <yunnk29@naver.com>
2026-04-24 18:43:36 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[101][101];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            int tmp;
+            cin >> tmp;
+
+            cout << arr[i][j] + tmp << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
