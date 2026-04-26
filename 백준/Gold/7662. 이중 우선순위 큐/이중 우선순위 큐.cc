8a5ce7b32bd3e9a9dd8484cedee36a55fcd2cb4e
ChoHyeon <yunnk29@naver.com>
2026-04-27 00:59:50 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1316 ms, Memory: 49016 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int k;
+		cin >> k;
+
+		multiset<int> dpq;
+
+		while (k--) {
+			char cmd;
+			cin >> cmd;
+
+			if (cmd == 'I') {
+				int n;
+				cin >> n;
+
+				dpq.insert(n);
+			}
+			else if (cmd == 'D') {
+				int n;
+				cin >> n;
+
+				if (n == 1 && !dpq.empty()) {
+					dpq.erase(prev(dpq.end()));
+				}
+				else if (n == -1 && !dpq.empty()) {
+					if (!dpq.empty()) dpq.erase(dpq.begin());
+				}
+			}
+		}
+
+		if (!dpq.empty()) {
+			cout << *prev(dpq.end()) << ' ' << *dpq.begin() << '\n';
+		}
+		else cout << "EMPTY\n";
+	}
+
+	return 0;
+}
